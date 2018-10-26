0cd61918d4cf35d99a4a3cc7585ea6dab6eecbcd
Tarek Radah <yenii@protonmail.ch>
2018-10-26 10:31:31 +0100

    Update MemDump1.cpp

--- MemDump1.cpp
+++ MemDump1.cpp
@@ -95,16 +95,16 @@ namespace MemDump{
 
 	VOID EnableDebugPriv()
 	{
-    	HANDLE hToken;
-    	LUID luid;
-    	TOKEN_PRIVILEGES tkp;
-    	OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
-    	LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &luid);
-    	tkp.PrivilegeCount = 1;
-    	tkp.Privileges[0].Luid = luid;
-    	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-    	AdjustTokenPrivileges(hToken, false, &tkp, sizeof(tkp), NULL, NULL);
-    	CloseHandle(hToken); 
+    		HANDLE hToken;
+    		LUID luid;
+    		TOKEN_PRIVILEGES tkp;
+    		OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
+    		LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &luid);
+    		tkp.PrivilegeCount = 1;
+    		tkp.Privileges[0].Luid = luid;
+    		tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+    		AdjustTokenPrivileges(hToken, false, &tkp, sizeof(tkp), NULL, NULL);
+    		CloseHandle(hToken); 
 	}
 
 	VOID MemDump(DWORD wPid, PCHAR szFileName)
